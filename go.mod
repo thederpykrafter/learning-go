f46cf5de66a3ad6556080ce3d515b10e49bfac02
thederpykrafter <thederpykrafter@gmail.com>
2024-08-08 16:48:51 -0400

    ansi

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module example/thederpykrafter/learning-go
 
 go 1.22.2
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
+	golang.org/x/term v0.22.0
+)
+
+require golang.org/x/sys v0.22.0 // indirect
--- learning-go.go
+++ learning-go.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"golang.org/x/term"
+	"strings"
+)
+
+func draw_line(char string, width int) {
+	fmt.Print(strings.Repeat(char, width))
+	fmt.Print("\n")
+}
+
+func draw_fill(char string, width int, height int) {
+	fmt.Print(strings.Repeat(char, width * height), "\n")
+}
+
+func move_cursor(line int, col int) {
+	fmt.Printf("\033[%d;%dH", line, col)
+	time.Sleep(1 * time.Millisecond)
+}
+
+func player_position() {
+	
+}
+
+func game_loop(width int, height int) {
+	// game loop
+	for range time.Tick(60 * time.Millisecond) {
+		fmt.Print("\033[2J") // clear screen
+		move_cursor(400,0) // move cursor home
+		draw_fill("%", width, height)
+
+		move_cursor(15, 13)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	// get terminal size
+	width, height, err := term.GetSize(0)
+	if err != nil {
+		return
+	}
+	fmt.Print(width, height)
+	
+	// enter alternate buffer
+	fmt.Print("\033[?1049h")
+	
+	game_loop(width, height)
 }
